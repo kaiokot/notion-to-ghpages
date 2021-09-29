5b1931059344a63fa60bee901ec314db5b230293
Kaio Turubia <kaio@stymeapp.com>
2021-09-29 02:21:38 -0300

    Now commiting working script 👍 😄

--- index.js
+++ index.js
@@ -5,35 +5,43 @@ let api = axios.create({
     baseURL: "https://www.notion.so"
 });
 
+const token = "token";
+const pageId = "pageId";
+const spaceId = "spaceId";
+
 api.defaults.headers.common["Content-Type"] = "application/json";
 api.defaults.headers.common["User-Agent"] = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:92.0) Gecko/20100101 Firefox/92.0";
 api.defaults.headers.common["Accept-Language"] = "en-US,en;q=0.5";
-api.defaults.headers.common["Cookie"] = "token_v2=TOKEN;";
-api.defaults.headers.common["x-notion-active-user-header"] = "USER";
+api.defaults.headers.common["Cookie"] = `token_v2=${token};`;
 
-async function exportPage() {
-
-    let exportRequest = {
-        task: {
-            eventName: "exportBlock",
-            request: {
-                block: {
-                    id: "c942ea9aae494524b51cb22c5cbfd179",
-                    spaceId: "c24b230d-7445-4d06-bf61-6a96f9b3cb02"
+async function getEnqueueRequest() {
+    return {
+        "task": {
+            "eventName": "exportBlock",
+            "request": {
+                "block": {
+                    "id": pageId,
+                    "spaceId": spaceId
                 },
-                exportOptions: {
-                    exportType: "html",
-                    locale: "en",
-                    timeZone: "America/Sao_Paulo"
+                "exportOptions": {
+                    "exportType": "html",
+                    "locale": "en",
+                    "timeZone": "America/Sao_Paulo"
                 },
-                recursive: false
+                "recursive": true
             }
         }
     }
+}
+
+async function exportPage() {
+
+    let exportRequest = await getEnqueueRequest();
 
     const responseEnqueueTask = await api.post("/api/v3/enqueueTask", exportRequest);
     let taskId = responseEnqueueTask.data.taskId;
-    console.log("taskID", taskId)
+
+    console.log("Enqueue taskID:", taskId)
 
     let taskIdsRequest = { "taskIds": [taskId] };
 
@@ -41,19 +49,17 @@ async function exportPage() {
 
     do {
         const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
-        await delay(5000) /// waiting 1 second.
+        await delay(2000) /// waiting 2 seconds.
 
         const responseGetTasks = await api.post("/api/v3/getTasks", taskIdsRequest);
 
         enqueueTaskState = responseGetTasks.data.results[0].state;
 
-        console.log(responseGetTasks.data);
-
         if (enqueueTaskState == "success") {
 
             let exportUrl = responseGetTasks.data.results[0].status.exportURL;
-
-            console.log("uow here is your url ", exportUrl);
+            console.log("uow here is your url \n", exportUrl);
+            break;
         }
 
     } while (enqueueTaskState !== "success");
